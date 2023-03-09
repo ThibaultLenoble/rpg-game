6a0b4043f81cf439974eb98700197a8aa35a6875
Thibault Lenoble <lenoblethibaultpro@gmail.com>
2023-03-09 08:58:32 +0100

    feat: Ajout prompt

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Hello RPG game</h1>
+  <div class="prompt">
+    <h1 class="prompt__title">Hello RPG game</h1>
+    <p class="prompt__description">Bienvenue sur RPG game, veuillez entrer votre nom pour commencer une partie</p>
+    <input type="text" class="prompt__input">
+    <button class="prompt__submit">Continuer</button>
   </div>
 `;
