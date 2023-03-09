e195c18fbf78ea3275cd1fe977af686b9948eaf0
LRAV-Alexis <alexis.grenier@lrav.fr>
2023-03-09 12:34:45 +0100

    Changement de selecteur + switch dataEvents

--- src/datas/events.ts
+++ src/datas/events.ts
@@ -1,9 +1,15 @@
 import RoomEvent from "../class/RoomEvent/RoomEvent";
 import { dataChoices } from "./choices";
 
-export const event1: RoomEvent = new RoomEvent(
-  "choice",
-  "Vous êtes dans une salle, vous pouvez choisir de partir en direction de la salle suivante ou de rester dans cette salle",
-  "Vous vous déplacez",
-  [dataChoices[0], dataChoices[0]]
-);
+export const dataEvents: RoomEvent[] = [
+  new RoomEvent(
+    "choice",
+    "Vous êtes dans une salle, vous pouvez choisir de partir en direction de la salle suivante ou de rester dans cette salle",
+    "Vous vous déplacez",
+    [dataChoices[0], dataChoices[1]]
+  ),
+  new RoomEvent("enigma", "Je suis un énigme", "Vous vous creusez la tête", [
+    dataChoices[0],
+    dataChoices[1],
+  ]),
+];
