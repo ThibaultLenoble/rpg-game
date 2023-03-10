76b1f50846ead081f5257fd22183370445c4292b
LRAV-Alexis <alexis.grenier@lrav.fr>
2023-03-10 10:49:12 +0100

    contenu

--- src/datas/choices.ts
+++ src/datas/choices.ts
@@ -2,7 +2,11 @@ import Choice from "../class/Choice/Choice";
 import ExchangeChoice from "../class/Choice/ExchangeChoice";
 import SimpleChoice from "../class/Choice/SimpleChoice";
 
-export const dataChoices: {MainEvent: Choice[], EnigmaEvent: SimpleChoice[], ExchangeEvent: ExchangeChoice[]} = {
+export const dataChoices: {
+  MainEvent: Choice[];
+  EnigmaEvent: SimpleChoice[];
+  ExchangeEvent: ExchangeChoice[];
+} = {
   MainEvent: [
     new Choice("Partir en direction de la salle suivante", "get-out"),
     new Choice(
@@ -14,11 +18,19 @@ export const dataChoices: {MainEvent: Choice[], EnigmaEvent: SimpleChoice[], Exc
     new SimpleChoice("Ouvrir le coffre", "chest-heal"),
     new SimpleChoice("Ouvrir le coffre", "chest-hit"),
     new SimpleChoice("Ouvrir le coffre", "chest-earn-money"),
+    new SimpleChoice(
+      "Ouvrir le coffre avec la clé posée au sol juste à côté",
+      "chest-hit"
+    ),
+    new SimpleChoice(
+      "Donner un coup de pied dans le coffre en espérant qu'il s'ouvre",
+      "chest-earn-money"
+    ),
   ],
   ExchangeEvent: [
     new ExchangeChoice("Echanger avec un autre joueur", "exchange"),
     new ExchangeChoice("Echanger avec un autre vendeur", "exchange"),
     new ExchangeChoice("Echanger avec un autre marchand", "exchange"),
     new ExchangeChoice("Echanger avec un autre magasinier", "exchange"),
-  ]
+  ],
 };
--- src/datas/events.ts
+++ src/datas/events.ts
@@ -1,20 +1,66 @@
 import RoomEvent from "../class/RoomEvent/RoomEvent";
-import {dataChoices} from "./choices";
+import { dataChoices } from "./choices";
 import EnigmaEvent from "../class/RoomEvent/EnigmaEvent";
 import MainEvent from "../class/RoomEvent/MainEvent";
 
-export const dataEvents: { mainEvents: MainEvent[], available: RoomEvent[] } = {
+export const dataEvents: { mainEvents: MainEvent[]; available: RoomEvent[] } = {
   mainEvents: [
-    new MainEvent(
-      "Aller à la salle suivante ?",
-      "",
-      [dataChoices.MainEvent[0]]
-    ),
+    new MainEvent("Aller à la salle suivante ?", "", [
+      dataChoices.MainEvent[0],
+    ]),
   ],
   available: [
     new EnigmaEvent(
-      'Vous arrivez dans une salle et vous voyez plusieurs coffre',
-      ''
-    )
-  ]
+      "Vous arrivez dans une salle et vous voyez plusieurs coffre",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous trouvez plusieurs malles en bois au centre de la pièce. Laquelle voulez-vous ouvrir ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous entrez dans une salle dont le sol est couvert de sable fin. Plusieurs urnes attirent votre attention. Quelle urne ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous entrez dans une salle dont le sol est couvert de poudre noire. Plusieurs urnes attirent votre attention. Quelle urne ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous entrez dans une salle dont le plafond est couvert de lait. Plusieurs urnes attirent votre attention. Quelle urne ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous êtes dans une pièce remplie d'armes anciennes et mystérieuses. Vous trouvez plusieurs boîtes sur une étagère en hauteur. Laquelle ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous entrez dans une grande salle avec un coffre doré au milieu. À côté, plusieurs coffres plus petits se trouvent. Lequel ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous arrivez dans une salle remplie de champignons étranges. Plusieurs coffres se trouvent dans la pièce. Lequel ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous arrivez dans une salle remplie de nains étranges. Plusieurs coffres se trouvent dans la pièce. Lequel ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous arrivez dans une salle remplie d'escargots. Plusieurs coffres se trouvent dans la pièce. Lequel ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous arrivez dans une salle remplie de lapins bleus. Plusieurs coffres se trouvent dans la pièce. Lequel ouvrez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Vous entrez dans une salle obscure. Vous discernez à peine un coffre sur votre gauche et une boîte en métal rouillé à votre droite. Que choisissez-vous ?",
+      ""
+    ),
+    new EnigmaEvent(
+      "Au détour d'un couloir, vous apercevez une lumière scintillante dans un coin de la pièce, quelques coffres sont entassés.",
+      ""
+    ),
+  ],
 };
